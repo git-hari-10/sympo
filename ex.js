86cdc41e73e788fdb2998a5ff0e6e4277e4b4943
HARI <144093230+Hackboy6010@users.noreply.github.com>
2024-10-11 12:23:34 +0530

    Add files via upload

---
+++ ex.js
@@ -0,0 +1,7 @@
+function showTechnicalDetails() {
+    alert("Technical Event --> Read instructions carefully and Enjoy Participating Good luck!");
+}
+
+function showNonTechnicalDetails() {
+    alert("Non-Technical Events --> Read instructions carefully and Enjoy Participating Good luck!");
+}
